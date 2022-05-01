ff4bc755c5705559953223db6a6cf0516fc00ce0
Sara Acuna <acunabenabidessaraelis@gmail.com>
2022-05-01 14:39:30 -0500

    Added current location

--- app/src/main/java/com/restaurantes/AllowlocationActivity.kt
+++ app/src/main/java/com/restaurantes/AllowlocationActivity.kt
@@ -1,10 +1,13 @@
 package com.restaurantes
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -12,6 +15,7 @@ import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -19,11 +23,22 @@ import com.google.android.gms.maps.model.MarkerOptions
 class AllowlocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map:GoogleMap
-    /*
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var locationPermissionGranted = false
+
+    private var lastKnownLocation: Location? = null
+
+
     companion object{
-        const val REQUEST_CODE_LOCATION = 0
+        private val TAG = AllowlocationActivity::class.java.simpleName
+        const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+        private const val DEFAULT_ZOOM = 15
     }
-    */
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.allowlocation)
@@ -47,8 +62,111 @@ class AllowlocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+
+
         createMarker()
+        enableLocation()
+
+
+        /*
+        updateLocationUI()
+        getDeviceLocation()
+        */
     }
+
+
+
+
+    //ACCESO A LA UBICACION CON LA DOCUMENTACION DE GOOGLE
+    /*
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        locationPermissionGranted = false
+        when(requestCode){
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (grantResults.isNotEmpty() &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    locationPermissionGranted = true
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+        updateLocationUI()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationUI() {
+        if (map == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                map?.isMyLocationEnabled = false
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            map?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+                        }
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        map?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        map?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+    */
+    //FIN DE ACCESO A LA UBICACION
+
+
+
+
+    //ACCESO A LA UBICACION CON EL VIDEO DE YOU TUBE https://www.youtube.com/watch?v=L6B8LUGp2VA&t=676s&ab_channel=Programaci%C3%B3nAndroidbyAristiDevs
 
     private fun createMarker() {
         val coordinates = LatLng(28.043893, -16.539329)
@@ -60,12 +178,13 @@ class AllowlocationActivity : AppCompatActivity(), OnMapReadyCallback {
             null
         )
     }
-    /*
+
     private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
         this,
         Manifest.permission.ACCESS_FINE_LOCATION
     ) == PackageManager.PERMISSION_GRANTED
 
+    @SuppressLint("MissingPermission")
     private fun enableLocation(){
         if (!::map.isInitialized) return
         if (isLocationPermissionGranted()){
@@ -81,11 +200,12 @@ class AllowlocationActivity : AppCompatActivity(), OnMapReadyCallback {
             Toast.makeText(this, "Por favor, active su ubicación en ajustes", Toast.LENGTH_SHORT).show()
         } else{
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-            REQUEST_CODE_LOCATION
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
             )
         }
     }
 
+    @SuppressLint("MissingPermission")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -93,12 +213,15 @@ class AllowlocationActivity : AppCompatActivity(), OnMapReadyCallback {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when(requestCode){
-           REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
                map.isMyLocationEnabled = true
            }else{
                Toast.makeText(this, "Para activar la ubicación dirigase a ajustes y acepte los permisos", Toast.LENGTH_SHORT).show()
            }
            else ->{}
        }
-    }*/
+    }
+
+    // FINAL DE ACCESO A LA UBICACION
+
 }
